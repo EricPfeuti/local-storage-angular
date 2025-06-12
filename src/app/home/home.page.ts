ccd18dc01a47ac1f1e8aa00e39bbc639fe1eed12
EricPfeuti <eric.pfeuti2007@gmail.com>
2025-06-12 08:41:19 -0300

    CRUD em Angular - Contatos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { ContatosService } from '../services/contatos.service';
+
+interface Contato {
+  nome: string;
+  email: string;
+  telefone: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -8,6 +15,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  contatos: Contato[] = [];
+  contato: Contato = { nome: '', email: '', telefone: '' };
+
+  indiceEdicao: number = -1;
+
+  constructor(private contatosService: ContatosService) {}
+
+  ngOnInit(){
+    this.carregarContatos();
+  }
+
+  async carregarContatos(){
+    this.contatos = await this.contatosService.obterContatos();
+  }
+
+  async salvarContato(){
+    if(this.indiceEdicao >= 0){
+      await this.contatosService.atualizarContato(this.indiceEdicao, this.contato);
+    } else {
+      await this.contatosService.adicionarContato(this.contato);
+    }
+    this.limparFormulario();
+    this.carregarContatos();
+  }
+
+  editarContato(indice: number){
+    this.contato = { ...this.contatos[indice] };
+    this.indiceEdicao = indice;
+  }
+
+  async excluirContato(indice: number){
+    await this.contatosService.excluirContato(indice);
+    this.carregarContatos();
+  }
+
+  limparFormulario(){
+    this.contato = { nome: '', email: '', telefone: '' };
+    this.indiceEdicao = -1;
+  }
 
 }
